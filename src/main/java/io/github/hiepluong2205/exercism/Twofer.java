d83f46f2abee685da950a9b767ac8ea03ab07089
hiepluong2205@gmail.com <hiepluong2205@gmail.com>
2022-01-17 00:06:50 -0800

    exercism

--- src/main/java/io/github/hiepluong2205/exercism/Twofer.java
+++ src/main/java/io/github/hiepluong2205/exercism/Twofer.java
@@ -2,8 +2,11 @@ package io.github.hiepluong2205.exercism;
 
 public class Twofer {
   public String twofer(String name) {
-    if (name == null) {}
-    String message = "";
+    if (name == null) {
+      name = "you";
+      System.out.println("name = " + name);
+    }
+    String message = "One for " + name + ", one for me.";
     return message;
   }
 }
--- src/test/java/io/github/hiepluong2205/exercism/TwoferTest.java
+++ src/test/java/io/github/hiepluong2205/exercism/TwoferTest.java
@@ -1,4 +1,11 @@
 package io.github.hiepluong2205.exercism;
 
+import org.testng.annotations.Test;
+
 public class TwoferTest {
+  @Test
+  public void testName() {
+      Twofer twofer = new Twofer();
+      twofer.twofer("");
+  }
 }
