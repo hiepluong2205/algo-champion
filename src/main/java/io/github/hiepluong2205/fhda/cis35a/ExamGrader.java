1e30665769cc3e3d4f31606263a21791d0c68746
Hiep Luong <Hiep.Luong@coxautoinc.com>
2022-02-01 21:34:21 -0800

    misc

---
+++ src/main/java/io/github/hiepluong2205/fhda/cis35a/ExamGrader.java
@@ -0,0 +1,36 @@
+package io.github.hiepluong2205.fhda.cis35a;
+/*
+Lab 2a -
+You must use arrays, static methods, loops and decision making constructs. Please put each part in its own project.
+Total points - 30.
+
+Part 2
+Your instructor has asked you to write a program that grades an exam. The exam has 15 multiple choice questions. Here are the correct answers:
+
+{"B", "D", "A", "A", "C", "D", "A", "B", "C", "A", "B", "C", "D", "A", "B"}
+
+A student must correctly answer 12 out of the 15 questions to pass the exam.
+Write a program that holds the correct answers to the exam in an array. The program should have an array that hold's the student's answers. The program should have the following static methods:
+a. passed() returns true if the student passed the exam or false if the student failed.
+b. totalCorrect() - Return the total number of correctly answered questions.
+c. totalIncorrect() - Returns the total number of incorrectly answered questions.
+d. An int array containing the questions numbers of the questions that the student missed.
+
+Demonstrate the program by setting the values of student's answers in main(You are not required to prompt the user to enter values using Scanner or System.in.read()), and then display the result returned from the methods written in the program.
+
+Here are some suggestions on variable and method declarations.
+char [] coorectAns = new char[15];
+char [] studentAns = new char[15];
+
+static boolean passed(char [] correct, char [] student) { }
+static int totalCorrectAnswers(char [] correct, char [] student) { }
+static int totalInCorrectAnswers(char [] correct, char [] student) { }
+static int [] questionNumbersMissed(char [] correct, char [] student) {
+//which questions were either not answered or were incorrect.
+//declaring a char array that keeps track of the questions that were incorrectly answered.
+}
+ */
+
+public class ExamGrader {
+    private static final String[] expected = {"B", "D", "A", "A", "C", "D", "A", "B", "C", "A", "B", "C", "D", "A", "B"};
+}
---
+++ src/main/java/io/github/hiepluong2205/fhda/cis35a/SalesManager.java
@@ -0,0 +1,28 @@
+package io.github.hiepluong2205.fhda.cis35a;
+/*
+Lab 2a -
+You must use arrays, static methods, loops and decision making constructs. Please put each part in its own project.
+Total points - 30.
+
+Part 3
+Write a program that lets the user enter four quarterly sales figures for six divisions of a company.
+The figures should be stored in a two dimensional array.
+Once the figures are entered the program should display the following for each quarter
+a. A list of the sales figures by division.
+b. Each division increases or decreases from the previous quarter(this will not be displayed for the first quarter).
+c. The total sales for the quarter.
+d. The company's increase or decrease from the previous quarter(this will not be displayed for the first quarter).
+e. The average sales for all divisions that quarter.
+f. The division with the highest sales for that quarter.
+*/
+
+public class SalesManager {
+    //Input Validation: Do not accept negative numbers for sales figures.
+    private static boolean isNegative(int salesFigure) {
+        if (salesFigure < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
