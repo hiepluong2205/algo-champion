6e9b5606835308ef94e975e5b991538ff494e45f
Hiep Luong <hiepluong2205@gmail.com>
2022-02-01 22:23:49 -0800

    misc

---
+++ src/test/java/io/github/hiepluong2205/leetcode/PowerArrayTest.java
@@ -0,0 +1,4 @@
+package io.github.hiepluong2205.leetcode;
+
+public class PowerArrayTest {
+}
--- src/test/java/io/github/hiepluong2205/meta/RotationalCipherTest.java
+++ src/test/java/io/github/hiepluong2205/meta/RotationalCipherTest.java
@@ -1,4 +1,4 @@
-package io.github.hiepluong2205.fb;
+package io.github.hiepluong2205.meta;
 
 // https://www.facebookrecruiting.com/portal/coding_practice_question/?problem_id=238827593802550&ppid=454615229006519&practice_plan=1
 public class RotationalCipherTest {
