99a2162936bd5768bd24777fbbf462a348f17e5e
Hiep Luong <hiepluong2205@gmail.com>
2022-02-02 14:50:52 -0800

    misc

--- src/main/java/io/github/hiepluong2205/fhda/cis35a/a2p2/NucleicAcid.java
+++ src/main/java/io/github/hiepluong2205/fhda/cis35a/a2p2/NucleicAcid.java
@@ -1,4 +1,4 @@
-package io.github.hiepluong2205.fhda.cis35a.a2p1;
+package io.github.hiepluong2205.fhda.cis35a.a2p2;
 
 /*
 https://docs.google.com/document/d/1GlIbmHnYvTkvgGkP7yLnSscTAzOUjWkoGi6pyzN1oo0/edit?usp=sharing
